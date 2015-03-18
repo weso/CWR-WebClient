4ad6a87ad04336bfd2c34ee17173039ea78ae2f1
Bernardo Martínez Garrido <programming@wandrell.com>
2015-03-18 09:43:56 +0100

    Deployment target version is now configurable.

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -2,15 +2,18 @@
 # This script deploys the application using the POM cofiguration
 # It is triggered only commits to the master or develop branches. Pulls are ignored
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_PYTHON_VERSION" == "2.7" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_PYTHON_VERSION" == "$DEPLOY_PYTHON_VERSION" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 
   gem install heroku
   git remote add heroku git@heroku.com:$HEROKU_APP.git
+
   echo "Host heroku.com" >> ~/.ssh/config
   echo "   StrictHostKeyChecking no" >> ~/.ssh/config
   echo "   CheckHostIP no" >> ~/.ssh/config
   echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config
+
   heroku keys:clear
+
   yes | heroku keys:add
   yes | git push heroku master
 
