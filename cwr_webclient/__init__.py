3275cb0a1f04c783dabaa19dc64c71b520a137c8
Bernardo Martínez Garrido <programming@wandrell.com>
2015-04-22 10:45:48 +0200

    Created factory method for the application.

--- cwr_webclient/__init__.py
+++ cwr_webclient/__init__.py
@@ -8,3 +8,43 @@
 """
 
 __version__ = '0.0.1'
+
+
+def _url_for_other_page(page):
+    from flask import url_for, request
+
+    args = request.view_args.copy()
+    args['page'] = page
+    return url_for(request.endpoint, **args)
+
+def create_app():
+    import os
+    import logging
+
+    from flask import Flask
+    from werkzeug.contrib.fixers import ProxyFix
+    from cwr_webclient.view import common_blueprint,cwr_blueprint,uso_blueprint
+
+    debug = bool(os.environ.get('DEBUG', True))
+    secret = os.environ.get('SECRET_KEY', 'development_key')
+
+    app = Flask(__name__)
+    app.register_blueprint(common_blueprint)
+    app.register_blueprint(cwr_blueprint, url_prefix='/cwr')
+    app.register_blueprint(uso_blueprint, url_prefix='/uso')
+
+    app.wsgi_app = ProxyFix(app.wsgi_app)
+
+    app.config['DEBUG'] = debug
+    app.config['SECRET_KEY'] = secret
+
+    if debug:
+        logging.basicConfig(level=logging.INFO)
+    else:
+        logging.basicConfig(filename='cwr.log', level=logging.INFO, maxBytes=10000, backupCount=1)
+
+    logging.info('Debug mode is set to %r' % debug)
+
+    app.jinja_env.globals['url_for_other_page'] = _url_for_other_page
+
+    return app
--- run_client.py
+++ run_client.py
@@ -1,44 +1,16 @@
 # -*- encoding: utf-8 -*-
+from cwr_webclient import create_app
 import os
-import logging
-
-from flask import url_for, request, Flask
-from werkzeug.contrib.fixers import ProxyFix
-from cwr_webclient.view import common_blueprint,cwr_blueprint,uso_blueprint
 
 __author__ = 'Bernardo Martínez Garrido'
 __license__ = 'MIT'
 __status__ = 'Development'
 
-def url_for_other_page(page):
-    args = request.view_args.copy()
-    args['page'] = page
-    return url_for(request.endpoint, **args)
-
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 33507))
     host = os.environ.get('HOST', '0.0.0.0')
-    debug = bool(os.environ.get('DEBUG', True))
-    secret = os.environ.get('SECRET_KEY', 'development_key')
 
-    app = Flask(__name__)
-    app.register_blueprint(common_blueprint)
-    app.register_blueprint(cwr_blueprint, url_prefix='/cwr')
-    app.register_blueprint(uso_blueprint, url_prefix='/uso')
-
-    app.wsgi_app = ProxyFix(app.wsgi_app)
-
-    app.config['DEBUG'] = debug
-    app.config['SECRET_KEY'] = secret
-
-    if debug:
-        logging.basicConfig(level=logging.INFO)
-    else:
-        logging.basicConfig(filename='cwr.log', level=logging.INFO, maxBytes=10000, backupCount=1)
-
-    logging.info('Debug mode is set to %r' % debug)
-
-    app.jinja_env.globals['url_for_other_page'] = url_for_other_page
+    app = create_app()
 
     app.run(host=host, port=port)
