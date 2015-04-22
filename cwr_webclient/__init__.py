b9ca810e4088b079f205c574fa9513747231b523
Bernardo Martínez Garrido <programming@wandrell.com>
2015-04-22 12:33:46 +0200

    Divided CWR views into concerns.

--- cwr_webclient/__init__.py
+++ cwr_webclient/__init__.py
@@ -24,7 +24,8 @@ def create_app():
 
     from flask import Flask
     from werkzeug.contrib.fixers import ProxyFix
-    from cwr_webclient.view import common_blueprint, cwr_blueprint, uso_blueprint
+    from cwr_webclient.view import common_blueprint, cwr_upload_blueprint, cwr_validation_blueprint, \
+        cwr_acknowledgement_blueprint, cwr_match_blueprint, uso_blueprint
 
     from cwr_webclient.uploads import __uploads__
 
@@ -33,7 +34,10 @@ def create_app():
 
     app = Flask(__name__)
     app.register_blueprint(common_blueprint)
-    app.register_blueprint(cwr_blueprint, url_prefix='/cwr')
+    app.register_blueprint(cwr_validation_blueprint, url_prefix='/cwr/validation')
+    app.register_blueprint(cwr_acknowledgement_blueprint, url_prefix='/cwr/acknowledgement')
+    app.register_blueprint(cwr_upload_blueprint, url_prefix='/cwr')
+    app.register_blueprint(cwr_match_blueprint, url_prefix='/cwr/match')
     app.register_blueprint(uso_blueprint, url_prefix='/uso')
 
     app.wsgi_app = ProxyFix(app.wsgi_app)
