983fd3a61882a2d31c20590663cdbd47bc56ac1e
Bernardo Martínez Garrido <programming@wandrell.com>
2015-04-24 11:45:37 +0200

    Removed USO module.

--- cwr_webclient/view/__init__.py
+++ cwr_webclient/view/__init__.py
@@ -6,4 +6,3 @@ from cwr_webclient.view.cwr.acknowledgement.routes import cwr_acknowledgement_bl
 from cwr_webclient.view.cwr.match.routes import cwr_match_blueprint
 from cwr_webclient.view.cwr.upload.routes import cwr_upload_blueprint
 from cwr_webclient.view.cwr.workload.routes import cwr_workload_blueprint
-from cwr_webclient.view.uso.routes import uso_blueprint
