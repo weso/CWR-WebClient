58953b7c8049591dc1d29d83f6fc5e8555f8c7de
Bernardo Martínez Garrido <programming@wandrell.com>
2015-04-27 12:30:02 +0200

    Removed workload section.

--- cwr_webclient/view/__init__.py
+++ cwr_webclient/view/__init__.py
@@ -5,5 +5,4 @@ from cwr_webclient.view.cwr.validation.routes import cwr_validation_blueprint
 from cwr_webclient.view.cwr.acknowledgement.routes import cwr_acknowledgement_blueprint
 from cwr_webclient.view.cwr.match.routes import cwr_match_blueprint
 from cwr_webclient.view.cwr.file.routes import cwr_file_blueprint
-from cwr_webclient.view.cwr.workload.routes import cwr_workload_blueprint
 from cwr_webclient.view.cwr.database.routes import cwr_database_blueprint
