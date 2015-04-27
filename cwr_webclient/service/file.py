320b973784952c27df50c3b8d41e8bcd2ae7d047
Bernardo Martínez Garrido <programming@wandrell.com>
2015-04-27 12:58:40 +0200

    Combined both file services.

--- cwr_webclient/service/file.py
+++ cwr_webclient/service/file.py
@@ -5,6 +5,7 @@ import os
 import datetime
 
 from werkzeug.utils import secure_filename
+from cwr.parser.file import CWRFileDecoder
 
 from cwr_webclient.model.file import CWRFileData
 from cwr_webclient.model.workload import WorkloadStatus
@@ -26,16 +27,32 @@ class FileService(object):
         raise NotImplementedError('The get_files method must be implemented')
 
     @abstractmethod
+    def get_file(self, id):
+        raise NotImplementedError('The get_data method must be implemented')
+
+    @abstractmethod
     def save_file(self, file, path):
         raise NotImplementedError('The save_file method must be implemented')
 
 
 class LocalFileService(FileService):
-    def __init__(self):
+    def __init__(self, path):
         super(FileService, self).__init__()
         self._files = [CWRFileData('File1', datetime.datetime.now(), WorkloadStatus.done),
                        CWRFileData('File2', datetime.datetime.now(), WorkloadStatus.done),
                        CWRFileData('File3', datetime.datetime.now(), WorkloadStatus.processing)]
+        self._files_data = {}
+        self._path = path
+        self._decoder = CWRFileDecoder()
+
+    def get_file(self, id):
+        if id in self._files_data:
+            data = self._files_data[id]
+        else:
+            data = self._read_cwr(id, self._path)
+            self._files_data[id] = data
+
+        return data
 
     def get_files(self):
         return self._files
@@ -45,3 +62,7 @@ class LocalFileService(FileService):
         file.save(os.path.join(path, filename))
 
         return file.filename
+
+    def _read_cwr(self, filename, path):
+        file_path = os.path.join(path, filename)
+        return self._decoder.decode(file_path)
