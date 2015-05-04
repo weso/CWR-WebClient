065aad1f61fee534366933e08643c8412a0ba551
Bernardo Martínez Garrido <programming@wandrell.com>
2015-05-04 09:16:17 +0200

    Small style corrections.

--- cwr_webclient/__init__.py
+++ cwr_webclient/__init__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 """
     CWR Web Client
-    ~~~~~~~~~~~~
+    ~~~~~~~~~~~~~~
     Client for CWR services.
     :copyright: (c) 2015 by WESO
     :license: MIT, see LICENSE for more details.
 """
 
 __version__ = '0.0.1'
+__license__ = 'MIT'
 
 
 def _url_for_other_page(page):
