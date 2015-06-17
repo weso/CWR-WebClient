e4d6f8ffc9c71a38b631234eb5994449ad60ccdd
Bernardo Martínez Garrido <programming@wandrell.com>
2015-06-17 09:14:39 +0200

    Fixed tests.

--- tests/service/test_cwr.py
+++ tests/service/test_cwr.py
@@ -12,10 +12,19 @@ __status__ = 'Development'
 
 class TestWSCWRServiceNoService(unittest.TestCase):
     def setUp(self):
-        self._service = WSCWRService('http://127.0.0.1:1', 'http://127.0.0.1:1')
+        self._service = WSCWRService('http://127.0.0.1:1', 'http://127.0.0.1:1', 'http://127.0.0.1:1')
 
     def test_process(self):
         file = StringIO.StringIO('text1')
         file.filename = 'filename'
 
         self._service.process(file)
+
+    def test_get_files(self):
+        self._service.get_files()
+
+    def test_get_file(self):
+        self._service.delete_file('abc')
+
+    def test_delete_file(self):
+        self._service.delete_file('abc')
