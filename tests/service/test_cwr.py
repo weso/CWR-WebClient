cbcc1871dcc913bfd60aadfa6f9782937c3c52cb
Bernardo Martínez Garrido <programming@wandrell.com>
2015-07-08 11:22:58 +0200

    Fixed tests.

--- tests/service/test_cwr.py
+++ tests/service/test_cwr.py
@@ -14,6 +14,7 @@ class TestWSCWRServiceNoService(unittest.TestCase):
     def setUp(self):
         self._service = WSCWRService('http://127.0.0.1:1', 'http://127.0.0.1:1',
                                      'http://127.0.0.1:1',
+                                     'http://127.0.0.1:1',
                                      'http://127.0.0.1:1')
 
     def test_process(self):
